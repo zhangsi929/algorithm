0753be105e6e58714f15cab6afb2a7a2dd4ba6e2
Si Zhang <szhang@SideMacBook-Pro.local>
2022-04-16 22:14:00 -0700

    follw up

--- Tree/doordash_max_tree_path_sum.py
+++ Tree/doordash_max_tree_path_sum.py
@@ -1,7 +1,5 @@
-
-from re import S
-from turtle import right
 from construct_binary_tree import ConstructBinaryTree
+import sys
 '''
 Doordash:
 Given a binary tree, find the maximum path sum from any two "alive nodes" within the tree. 
@@ -20,14 +18,9 @@ What if any nodes in the tree can be alive instead of just the leaves
 case1 = [5, 2, 0, 25, None, 14, 15]
 root1 = ConstructBinaryTree.construct_binary_tree_by_horizontal_list(case1)
 
-# class TreeNode:
-#     def __init__(self, val) -> None:
-#         self.val = val
-#         self.left, self.right = None, None
-
 class Solution:
     def findMaxSum(self, root):
-        self.max_sum = 0
+        self.max_sum = -sys.maxsize - 1
         if not root:
             return 0
         self.helper(root)
@@ -40,6 +33,25 @@ class Solution:
         right_max_sum_to_leaf = self.helper(root.right)
         self.max_sum = max(self.max_sum, root.val + left_max_sum_to_leaf + right_max_sum_to_leaf)
         return max(left_max_sum_to_leaf + root.val, right_max_sum_to_leaf + root.val)
+
+#follow up
+class Solution2:
+    def maxPathSum(self, root) -> int:
+        if not root:
+            return 0
+        self.max_sum = root.val
+        self.helper(root)
+        return self.max_sum
+    
+    def helper(self, root):
+        if not root:
+            return 0
+        l = self.helper(root.left)
+        r = self.helper(root.right)
+        self.max_sum = max(self.max_sum, l + r + root.val)
+        if l + r + root.val < 0:
+            return 0
+        return max(l + root.val, r + root.val)
 
 solution = Solution()
 result = solution.findMaxSum(root1)
